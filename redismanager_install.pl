30919130c37c64c5724e258bb4c085474f77a442
ed511911 <95634890+ed511911@users.noreply.github.com>
2023-02-28 00:03:29 +0200

    Update redismanager_install.pl

--- redismanager_install.pl
+++ redismanager_install.pl
@@ -20,35 +20,35 @@ print CYAN " - Done\n";
 
 # Obtain the plugin from Github
 print YELLOW "Downloading redismanager.tar.gz... ";
-Cpanel::SafeRun::Timed::timedsaferun( 4, 'wget', '-O', '/root/redismanager.tar.gz', '-o', '/dev/null', "https://raw.githubusercontent.com/cPanelPeter/rblcheck/master/redismanager/redismanager.tar.gz" );
+Cpanel::SafeRun::Timed::timedsaferun( 4, 'wget', '-O', '/root/redismanager.tar.gz', '-o', '/dev/null', "https://github.com/ed511911/redismanager/redismanager.tar.gz" );
 print CYAN " - Done\n";
 
-# Uncompress whmrblcheck.tar.gz
-print YELLOW "Extracting whmrblcheck.tar.gz... ";
+# Uncompress redismanager.tar.gz
+print YELLOW "Extracting redismanager.tar.gz... ";
 my $tar = Archive::Tar->new;
 $tar->read("/root/whmrblcheck.tar.gz");
 $tar->extract();
 print CYAN " - Done\n";
 
-# Copy the rblcheck.jpg (Icon) image file to /usr/local/cpanel/whostmgr/docroot/addon_plugins
-print YELLOW "Copying rblcheck.jpg to /usr/local/cpanel/whostmgr/docroot/addon_plugins... ";
-move("/root/rblcheck.jpg","/usr/local/cpanel/whostmgr/docroot/addon_plugins") or die "Copy failed: $!";
+# Copy the redismanager.jpg (Icon) image file to /usr/local/cpanel/whostmgr/docroot/redismanager
+print YELLOW "Copying redismanager.jpg to /usr/local/cpanel/whostmgr/docroot/redismanager... ";
+move("/root/redismanager.jpg","/usr/local/cpanel/whostmgr/docroot/redismanager") or die "Copy failed: $!";
 print CYAN " - Done\n";
 
-# Copy the rblcheck.cgi file to /usr/local/cpanel/whostmgr/docroot/cgi/rblcheck
-print YELLOW "Copying rblcheck.cgi to /usr/local/cpanel/whostmgr/docroot/cgi/rblcheck... ";
-move("/root/rblcheck.cgi","/usr/local/cpanel/whostmgr/docroot/cgi/rblcheck") or die "Copy failed: $!";
+# Copy the redismanager.cgi file to /usr/local/cpanel/whostmgr/docroot/cgi/redismanager
+print YELLOW "Copying redismanager.cgi to /usr/local/cpanel/whostmgr/docroot/cgi/redismanager... ";
+move("/root/redismanager.cgi","/usr/local/cpanel/whostmgr/docroot/cgi/redismanager") or die "Copy failed: $!";
 print CYAN " - Done\n";
 
-# Set execute permissions on the rblcheck.cgi script
-print YELLOW "Setting permissions on /usr/local/cpanel/whostmgr/docroot/cgi/rblcheck/rblcheck.cgi to 0755...";
-chmod 0755, "/usr/local/cpanel/whostmgr/docroot/cgi/rblcheck/rblcheck.cgi";
+# Set execute permissions on the redismanager.cgi script
+print YELLOW "Setting permissions on /usr/local/cpanel/whostmgr/docroot/cgi/redismanager/redismanager.cgi to 0755...";
+chmod 0755, "/usr/local/cpanel/whostmgr/docroot/cgi/redismanager/redismanager.cgi";
 print CYAN " - Done\n";
 
 # Register the plugin
 print YELLOW "Registering plugin...";
-system( "/usr/local/cpanel/bin/register_appconfig /root/rblcheck.conf" );
+system( "/usr/local/cpanel/bin/register_appconfig /root/redismanager.conf" );
 
 print GREEN "\nRBL Check WHM Plugin installed!\n";
-unlink( "/root/whmrblcheck.tar.gz" );
+unlink( "/root/redismanager.tar.gz" );
 exit;
