3be8fa342f8481d99aa4695f029fad4c45e0673f
ed511911 <95634890+ed511911@users.noreply.github.com>
2023-02-27 23:50:29 +0200

    Update redismanager_install.pl

--- redismanager_install.pl
+++ redismanager_install.pl
@@ -1,1 +1,54 @@
 
+#!/usr/local/cpanel/3rdparty/bin/perl
+
+use Net::IP;
+use IO::Uncompress::Gunzip qw(gunzip $GunzipError);
+use File::Copy;
+use Archive::Tar;
+use Cpanel::SafeRun::Timed ();
+use Cpanel::SafeRun::Errors();
+use Term::ANSIColor qw(:constants);
+$Term::ANSIColor::AUTORESET = 1;
+
+system("clear");
+print GREEN "Installing the Redis Manager WHM Plugin...\n";
+
+# Create the directory for the plugin
+print YELLOW "Creating /usr/local/cpanel/whostmgr/docroot/cgi/redismanager directory... ";
+mkdir "/usr/local/cpanel/whostmgr/docroot/cgi/redismanager";
+print CYAN " - Done\n";
+
+# Obtain the plugin from Github
+print YELLOW "Downloading redismanager.tar.gz... ";
+Cpanel::SafeRun::Timed::timedsaferun( 4, 'wget', '-O', '/root/redismanager.tar.gz', '-o', '/dev/null', "https://raw.githubusercontent.com/cPanelPeter/rblcheck/master/redismanager/redismanager.tar.gz" );
+print CYAN " - Done\n";
+
+# Uncompress whmrblcheck.tar.gz
+print YELLOW "Extracting whmrblcheck.tar.gz... ";
+my $tar = Archive::Tar->new;
+$tar->read("/root/whmrblcheck.tar.gz");
+$tar->extract();
+print CYAN " - Done\n";
+
+# Copy the rblcheck.jpg (Icon) image file to /usr/local/cpanel/whostmgr/docroot/addon_plugins
+print YELLOW "Copying rblcheck.jpg to /usr/local/cpanel/whostmgr/docroot/addon_plugins... ";
+move("/root/rblcheck.jpg","/usr/local/cpanel/whostmgr/docroot/addon_plugins") or die "Copy failed: $!";
+print CYAN " - Done\n";
+
+# Copy the rblcheck.cgi file to /usr/local/cpanel/whostmgr/docroot/cgi/rblcheck
+print YELLOW "Copying rblcheck.cgi to /usr/local/cpanel/whostmgr/docroot/cgi/rblcheck... ";
+move("/root/rblcheck.cgi","/usr/local/cpanel/whostmgr/docroot/cgi/rblcheck") or die "Copy failed: $!";
+print CYAN " - Done\n";
+
+# Set execute permissions on the rblcheck.cgi script
+print YELLOW "Setting permissions on /usr/local/cpanel/whostmgr/docroot/cgi/rblcheck/rblcheck.cgi to 0755...";
+chmod 0755, "/usr/local/cpanel/whostmgr/docroot/cgi/rblcheck/rblcheck.cgi";
+print CYAN " - Done\n";
+
+# Register the plugin
+print YELLOW "Registering plugin...";
+system( "/usr/local/cpanel/bin/register_appconfig /root/rblcheck.conf" );
+
+print GREEN "\nRBL Check WHM Plugin installed!\n";
+unlink( "/root/whmrblcheck.tar.gz" );
+exit;
